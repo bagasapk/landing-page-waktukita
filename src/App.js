8e85b22743479555d7fbb9093043a9fb7ab9b17b
Calvin Wijaya <47976609+crshao@users.noreply.github.com>
2021-06-10 22:02:47 +0700

    Login container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
+import logo1 from "./resources/images/logo1.svg";
+import "./register.css";
 import './App.css';
 import * as ReactBootStrap from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="white" variant="light">
+      {/* <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="white" variant="light">
         <ReactBootStrap.Navbar.Brand href="#home">Waktukita.com</ReactBootStrap.Navbar.Brand>
         {/* <ReactBootStrap.Navbar bg="dark">
           <ReactBootStrap.Navbar.Brand href="#home">
@@ -18,21 +20,64 @@ function App() {
             />
           </ReactBootStrap.Navbar.Brand> */}
         {/* </ReactBootStrap.Navbar> */}
-        <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        {/* <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
           <ReactBootStrap.Nav className="mr-auto">
           </ReactBootStrap.Nav>
           <ReactBootStrap.Nav>
             <ReactBootStrap.Nav.Link href="#test">Login sebagai ?</ReactBootStrap.Nav.Link>
             <ReactBootStrap.Button variant="outline-info">Student</ReactBootStrap.Button>{' '}
-            <ReactBootStrap.Button variant="info">Educator</ReactBootStrap.Button>{' '}
+            <ReactBootStrap.Button variant="info">Educator</ReactBootStrap.Button>{' '} */}
 
             {/* <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
               Educator
             </ReactBootStrap.Nav.Link> */}
-          </ReactBootStrap.Nav>
-        </ReactBootStrap.Navbar.Collapse>
-      </ReactBootStrap.Navbar>
+          {/* </ReactBootStrap.Nav> */}
+        {/* </ReactBootStrap.Navbar.Collapse> */}
+      {/* </ReactBootStrap.Navbar> */}
+      <div className="registerContainer">
+          <div className="navbarRegister">
+            <img src={logo1}></img>
+            <div className="col-lg-6">
+              <div className="buttonFlexNav">
+                <span
+                  style={{ color: "#404041", opacity: "0.5" }}
+                  className="textButton "
+                >
+                  Login Sebagai ?
+                </span>
+                <div className="col-lg-6">
+                  <div className="buttonFlexNav">
+                    <button className="navButtonPrimary1">
+                      <span style={{ color: "#00A69C" }} className="textButton">
+                        Student
+                      </span>
+                    </button>
+                    <button className="navButtonPrimary2">
+                      <span style={{ color: "#FFF" }} className="textButton">
+                        Educator
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      <section className="login">
+        <div className="loginContainer">
+          <label>Email</label>
+          <input
+            type="text"
+            autoFocus
+            required
+          />
+          <label>Kata Sandi</label>
+          <input
+            type="password" required/>
+        </div>
+      </section>
+    
     </div>
   );
 }
