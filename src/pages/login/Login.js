b68bbdde9ce0725ba16a1aa7f761ead52d7b192d
bagasapk <bagasapk>
2021-06-24 20:24:40 +0700

    Handler login + google, responsive navLogin

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import { withRouter } from "react-router";
 import NavbarLogin from "./navbar/navbar";
 import hide from "../../resources/symbols/hide.svg";
 import symbol from "../../resources/symbols/XMLID1386(active).svg";
@@ -7,23 +8,85 @@ import googleSymbol from "./google.svg";
 import "./testcss.css";
 import "./login1.css";
 import "./login.css";
+import authService from "../../services/auth.service";
 
 //Login
 // import ReactDOM from 'react-dom';
 import { GoogleLogin } from "react-google-login";
 import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 
-const responseGoogle = (response) => {
-  console.log(response);
+
+
+const responseGoogleFailure = (response) => {
+  console.log("Login Gagal :", response);
 };
+
+const clientId =
+  "123329037849-o2qjarpmvrpn7r9ue1b99hskvmaj1r2i.apps.googleusercontent.com";
 const responseFacebook = (response) => {
   console.log(response);
 };
 
-const Login = () => {
+const Login = (props) => {
+  const responseGoogleSuccess = (response) => {
+    console.log("Login Success :", response);
+    if (response.accessToken) {
+      localStorage.setItem("user", JSON.stringify(response.accessToken));
+    }
+    props.history.push('/');
+    window.location.reload();
+  };
+
+  const form = useRef();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [, setMessage] = useState("");
+  // const [loading,setLoading] = useState(false);
+  // const [email,setEmail] = useState("");
+  const onChangeEmail = (e) => {
+    const email = e.target.value;
+    setEmail(email);
+  };
+
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
   const [passwordShown, setPasswordShown] = useState(false);
   const togglePasswordVisibility = () => {
     setPasswordShown(passwordShown ? false : true);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("email :", email, "password :", password);
+
+    //   setMessage("");
+    // setLoading(true);
+
+    // form.validateAll();
+
+    //   if (checkBtn.current.context._errors.length === 0) {
+    authService.login(email, password).then(
+      () => {
+        props.history.push("/profile");
+        window.location.reload();
+      },
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        // setLoading(false);
+        setMessage(resMessage);
+      }
+    );
+    //   } else {
+    //     setLoading(false);
+    //   }
   };
 
   return (
@@ -42,7 +105,7 @@ const Login = () => {
                 style={{ width: "20px", height: "45px" }}
                 src={symbol}
               ></img>
-              <form>
+              <form onSubmit={handleLogin} ref={form}>
                 <div
                   style={{ textAlign: "left" }}
                   className="mx-auto d-flex flex-column"
@@ -52,6 +115,9 @@ const Login = () => {
                     <input
                       type="email"
                       placeholder="john.doe@gmail.com"
+                      value={email}
+                      onChange={onChangeEmail}
+                      required
                     ></input>
                   </div>
                   <div className="pb-3">
@@ -60,6 +126,9 @@ const Login = () => {
                       id="password"
                       type={passwordShown ? "text" : "password"}
                       placeholder="************"
+                      value={password}
+                      onChange={onChangePassword}
+                      required
                     ></input>{" "}
                     <i
                       style={{ marginLeft: "-30px", cursor: "pointer" }}
@@ -110,7 +179,7 @@ const Login = () => {
                   callback={responseFacebook}
                 />
                 <GoogleLogin
-                  clientId="295513891719-1rscphpsbern9hb147h807u4sdu6fok9.apps.googleusercontent.com"
+                  clientId={clientId}
                   render={(renderProps) => (
                     <button
                       className="googleButton mb-5"
@@ -126,8 +195,8 @@ const Login = () => {
                     </button>
                   )}
                   buttonText="Login"
-                  onSuccess={responseGoogle}
-                  onFailure={responseGoogle}
+                  onSuccess={responseGoogleSuccess}
+                  onFailure={responseGoogleFailure}
                   cookiePolicy={"single_host_origin"}
                 />
                 {/* <button className="navButton googleButton mb-5">
@@ -141,4 +210,4 @@ const Login = () => {
     </div>
   );
 };
-export default Login;
+export default withRouter(Login);
